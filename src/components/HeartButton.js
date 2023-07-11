6f17f577c92a30561cbb6c7d3c785da87db54fca
yejiniee <02doyoung01@naver.com>
2023-07-11 16:22:39 +0900

    찜버튼 활성화

---
+++ src/components/HeartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../styles/ContentDetail.module.css";
+
+const HeartButton = ({ heart, onClick }) => {
+  return (
+    <div
+      className={heart ? styles.btn_unheart : styles.btn_heart}
+      onClick={onClick}
+    >
+      <div className={heart ? styles.div17 : styles.div177}>
+        {heart ? <p>찜해제</p> : <p>찜하기</p>}
+      </div>
+    </div>
+  );
+};
+
+export default HeartButton;
