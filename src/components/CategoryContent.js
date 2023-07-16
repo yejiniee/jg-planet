1b56995e04fd154003d8959344ea6b84c9f699f1
seojungyoo <seojungyoo22@gmail.com>
2023-07-16 23:53:39 +0900

    아코디언 작동문제 수정

--- src/components/CategoryContent.js
+++ src/components/CategoryContent.js
@@ -4,6 +4,10 @@ import { motion } from "framer-motion";
 import styles from "../styles/Category.module.css"
 
 export const CategoryContent = ({ text = "여성패션"}) => {
+    const handleCheckboxClick = (event) => {
+        event.stopPropagation();
+      };
+
     return (
     <motion.div layout
         initial={{ opacity: 0 }}
@@ -11,7 +15,7 @@ export const CategoryContent = ({ text = "여성패션"}) => {
         exit={{ opacity: 0 }}
     >
         <div className={styles.categoryContent}>
-            <input type="checkbox" />
+            <input type="checkbox" onClick={handleCheckboxClick}  />
             <div className={styles.cLabel}>{text}</div>
         </div>
     </motion.div>
